0c7c3c1ab8941d547f3c251ac843c6de1530fed0
FloxKcah <flox@ratgers.nl>
2025-04-11 10:43:15 +0200

    Added test.py

---
+++ test.py
@@ -0,0 +1,51 @@
+import os
+from pynput import keyboard
+import google.generativeai as genai
+from PIL import ImageGrab, Image
+
+GOOGLE_API_KEY = "AIzaSyBolqZnY-x--htJt1V6r86oXMIeZZfqm2s"
+GEMINI_MODEL_ID = "gemini-2.0-flash"
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel(GEMINI_MODEL_ID)
+
+def send_notification(title, message):
+    print(f"{title} - {message}")
+    command = f'notify-send "{title}" "{message}"'
+    os.system(command)
+
+def screenshot(name):
+    screenshot = ImageGrab.grab()
+    screenshot.save(name)
+    return name
+
+def null(key): pass
+
+def analyze_image(image_path):
+    try:
+        img = Image.open(image_path)
+        response = model.generate_content(["On this image is a math problem if not say ONLY 'error 1'(with out ') if there is 1 awnser needed(example): 13 and if for examople 2: 15, 94 and if its with remainder(for example): 12 remainder: 2. and if its 2 times with remainder(for example): 12 remainder: 2, 15 remainder: 3", img])
+        response.resolve()
+        if response.text == "error 1":
+            return "Error!", "No math problem found!"
+        else:
+            os.system(f"python win.py '{response.text}' > /dev/null 2>&1 &")
+            return "Awnser(s) Found!", response.text
+    except Exception as e:
+        return "Error!", f"Error analyzing image: {str(e)}!"
+
+def on_press(key):
+    if key == keyboard.Key.f8:
+        print("You pressed 'f8'!")
+        shot_name = screenshot("screenshot.png")
+        
+        if os.path.exists(shot_name):
+            send_notification("Loading...", "Letting the magic happen!")
+            title, desc = analyze_image(shot_name)
+            send_notification(title, desc)
+        else:
+            print(f"Image file not found: {shot_name}")
+
+with keyboard.Listener(on_press=on_press, on_release=null) as listener:
+    listener.join()
